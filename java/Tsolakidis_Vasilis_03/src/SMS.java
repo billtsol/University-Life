b5550b275eea202f945e1068ce9913f44799ac2a
billtsol <billtsol@protonmail.com>
2022-12-14 15:32:00 +0200

    2.0 suspect Version

---
+++ java/Tsolakidis_Vasilis_03/src/SMS.java
@@ -0,0 +1,26 @@
+public class SMS extends Communication {
+    private String message;
+
+    public SMS(String phoneNumber1, String phoneNumber2, int year, int month, int day, String message) {
+        super(phoneNumber1, phoneNumber2, year, month, day);
+        this.message = message;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("This SMS has the following info");
+        super.printInfo();
+        System.out.println("Text: " + this.message);
+    }
+
+    @Override
+    public int getDuration() {
+        return -1;
+    }
+
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
---
+++ java/Tsolakidis_Vasilis_03/src/Suspect.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class Suspect {
+    private String name;
+    private String username;
+    private String country;
+    private String city;
+
+    ArrayList<String> phoneList = new ArrayList<String>();
+    ArrayList<Suspect> isConnected = new ArrayList<Suspect>();
+
+    public Suspect(String name, String username, String country, String city) {
+        this.name = name;
+        this.username = username;
+        this.country = country;
+        this.city = city;
+    }
+
+    // Getters
+    public String getCountry() {
+        return this.country;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCodeName() {
+        return this.username;
+    }
+
+    public ArrayList<String> getPhoneList() {
+        return this.phoneList;
+    }
+
+    public ArrayList<Suspect> getConnectedList() {
+        return this.isConnected;
+    }
+
+    // Methods
+    public void addNumber(String number) {
+        this.phoneList.add(number);
+    }
+
+    public void addNewSupect(Suspect aSuspect) {
+        if (!isConnectedTo(aSuspect)) {
+            isConnected.add(aSuspect);
+        }
+    }
+
+    public boolean isConnectedTo(Suspect aSuspect) {
+        return this.isConnected.contains(aSuspect);
+    }
+
+    public ArrayList<Suspect> getCommonPartners(Suspect aSuspect) {
+        ArrayList<Suspect> results = new ArrayList<Suspect>();
+
+        for (Suspect suspect : aSuspect.getConnectedList()) {
+            if (this.isConnected.contains(suspect)) {
+                results.add(suspect);
+            }
+        }
+        return results;
+    }
+
+    public void printConnections() {
+        for (Suspect sp : isConnected) {
+            if (sp.getCountry().equals(this.country)) {
+                System.out.println("Name: " + sp.getName() + " Username: " + sp.getCodeName() + "*");
+
+            } else {
+                System.out.println("Name: " + sp.getName() + " Username: " + sp.getCodeName());
+            }
+        }
+    }
+
+}
