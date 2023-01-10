6166e1c7f33e63b35c7224e508f2b807b21bc457
billtsol <billtsol@protonmail.com>
2023-01-10 05:32:01 +0200

    problem solved

--- java/Tsolakidis_Vasilis_4/src/VisualizeNetworkGUI.java
+++ java/Tsolakidis_Vasilis_4/src/VisualizeNetworkGUI.java
@@ -3,11 +3,13 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.FRLayout;
 import edu.uci.ics.jung.algorithms.layout.KKLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseGraph;
 import edu.uci.ics.jung.graph.event.GraphEvent.Edge;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import edu.uci.ics.jung.samples.SimpleGraphDraw;
@@ -16,6 +18,7 @@ import edu.uci.ics.jung.visualization.VisualizationImageServer;
 import edu.uci.ics.jung.visualization.decorators.EdgeShape;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.visualization.renderers.EdgeArrowRenderingSupport;
+import edu.uci.ics.jung.visualization.renderers.VertexLabelRenderer;
 
 public class VisualizeNetworkGUI<V, E> extends JFrame {
 
@@ -25,55 +28,31 @@ public class VisualizeNetworkGUI<V, E> extends JFrame {
 
         this.registry = registry;
 
-        Graph<Integer, String> g = new DirectedSparseGraph<>();
+        Graph<String, String> g = new SparseGraph<>();
 
-        // /*
-        // * g.addVertex("Vertex1");
-        // * g.addVertex("Vertex2");
-        // * g.addVertex("Vertex3");
-        // * g.addEdge("Edge1", "Vertex1", "Vertex2");
-        // * g.addEdge("Edge2", "Vertex1", "Vertex3");
-        // * g.addEdge("Edge3", "Vertex3", "Vertex1");
-        // */
+        for (Suspect suspect : registry.getSuspectList()) {
+            g.addVertex(suspect.getCodeName());
+        }
 
-        // for (Suspect suspect : registry.getSuspectList()) {
-        // g.addVertex(suspect.getCodeName());
-        // // ToStringLabeller label = new ToStringLabeller();
-        // }
+        int counter = 0;
+        for (Suspect suspect : registry.getSuspectList()) {
+            for (Suspect partner : suspect.getConnectedList()) {
+                g.addEdge("Edge" + Integer.toString(counter), suspect.getCodeName(), partner.getCodeName());
+                counter++;
+            }
+        }
 
-        // int number = 1;
-        // // for (Suspect suspect : registry.getSuspectList()) {
-        // // for (Suspect partern : suspect.getConnectedList()) {
-        // // String edge = "Edge" + Integer.toString(number);
-        // // g.addEdge("Edge1", suspect.getCodeName(), partern.getCodeName());
-        // // }
-        // // number++;
-        // // }
-
-        // g.addEdge("Edge1", registry.getSuspectList().get(0).getCodeName(),
-        // registry.getSuspectList().get(0).getConnectedList().get(0).getCodeName());
-        // // g.addEdge("Edge2", "Vertex1", "Vertex3");
-        // // g.addEdge("Edge3", "Vertex3", "Vertex1");
-
-        // VisualizationImageServer<V, E> vs;
-
-        // vs = new VisualizationImageServer<V, E>(new KKLayout(g), new Dimension(500,
-        // 500));
-
-        g.addVertex(1);
-        g.addVertex(2);
-        g.addVertex(3);
-
-        g.addEdge("Edge1", 1, 2);
-        g.addEdge("Edge2", 1, 3);
-
-        Layout<Integer, String> layout = new KKLayout<Integer, String>(g);
+        // Layout<String, String> layout = new KKLayout<String, String>(g);
+        Layout<String, String> layout = new FRLayout<String, String>(g);
         layout.setSize(new Dimension(350, 350));
-        BasicVisualizationServer<Integer, String> vv = new BasicVisualizationServer<Integer, String>(layout);
-        vv.setPreferredSize(new Dimension(350, 350));
+
+        BasicVisualizationServer<String, String> vv = new BasicVisualizationServer<String, String>(layout);
+        vv.setPreferredSize(new Dimension(550, 550));
+
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<String>());
 
         getContentPane().add(vv);
-        this.setSize(500, 500);
+        this.setSize(550, 550);
         this.setLocationRelativeTo(null);
         this.pack();
         this.setVisible(true);
